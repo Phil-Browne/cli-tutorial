fbf85975bf6aa04c31958a5330d3431f2bb85247
MegaportPhilipBrowne <philip.browne@megaport.com>
2026-04-02 12:33:02 -0700

    feat: render mermaid code blocks as diagrams via ProseCode override

---
+++ components/content/ProseCode.vue
@@ -0,0 +1,14 @@
+<template>
+  <NetworkDiagram v-if="language === 'mermaid'" :definition="code ?? ''" />
+  <slot v-else />
+</template>
+
+<script setup lang="ts">
+defineProps<{
+  code?: string
+  language?: string
+  filename?: string
+  highlights?: number[]
+  meta?: string
+}>()
+</script>
