a77b35c82d48b34f3519c1398a0121fe91464640
MegaportPhilipBrowne <philip.browne@megaport.com>
2026-04-02 15:38:34 -0700

    fix: add Go install prerequisite note to quick start

--- pages/index.vue
+++ pages/index.vue
@@ -234,7 +234,7 @@ const quickStart = [
     num: 1,
     label: 'Install',
     command: 'go install github.com/megaport/megaport-cli@latest',
-    description: 'Requires Go 1.21+',
+    description: 'Install Go first from go.dev/dl',
   },
   {
     num: 2,
